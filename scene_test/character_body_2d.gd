1d7da1a39088c0c35f18d874e7a70f53e53cffc2
Kurby <andras.bukovics@gmail.com>
2026-07-02 16:07:30 +0200

    improved physics

--- scene_test/character_body_2d.gd
+++ scene_test/character_body_2d.gd
@@ -2,8 +2,9 @@ extends CharacterBody2D
 
 @export var gravity_growth = 0.01
 var gravity = 5
-var SPEED = 300.0
-var JUMP_VELOCITY = -200.0
+var speed = 200.0
+var acceleration = 50
+var jump_velocity = -200.0
 
 
 func _physics_process(_delta):
@@ -14,17 +15,17 @@ func _physics_process(_delta):
 		velocity.y += gravity
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * speed, acceleration)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, acceleration)
 
 	move_and_slide()
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	area.get_parent().call_deferred("queue_free")
-	JUMP_VELOCITY -= 100
+	jump_velocity -= 100
