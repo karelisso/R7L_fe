9233891503af74165c8e465dda876d9cd342d26b
karelisso <50169626+karelisso@users.noreply.github.com>
2026-07-02 16:48:17 +0200

    walljump

--- scene_test/character_body_2d.gd
+++ scene_test/character_body_2d.gd
@@ -6,18 +6,26 @@ var SPEED = 150.0
 var JUMP_VELOCITY = -200.0
 @onready var anim = $AnimatedSprite2D
 
+var can_walljump = true
+var walljump = true
+var can_dash = true
+
 func _init() -> void:
 	gravity = 5.0
 	gravity_growth = 0.01
 
 func _physics_process(_delta):
-	
+	var direction := Input.get_axis("left", "right")
 	gravity += gravity_growth
 	
 	if not is_on_floor():
 		velocity.y += gravity
+		if walljump and velocity.x ==0 and Input.is_action_just_pressed("jump") and abs(direction) > 0.2:
+			velocity.y = JUMP_VELOCITY
+			walljump = false
 		anim.play("spin")
 	else:
+		walljump = true
 		if velocity.x >0.2:
 			anim.play("walkrigh")
 		elif velocity.x < -0.2:
@@ -27,7 +35,6 @@ func _physics_process(_delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
@@ -40,5 +47,5 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 	area.call_deferred("loadlevel")
 	#JUMP_VELOCITY -= 100
 	gravity -= 2
-	if gravity >5:
+	if gravity <5:
 		gravity = 5
