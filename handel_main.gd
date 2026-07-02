535891b79b732218920a85ec4ccd2510646000f0
karelisso <50169626+karelisso@users.noreply.github.com>
2026-07-02 14:54:42 +0200

    scene maganger is here, call it with signals

--- handel_main.gd
+++ handel_main.gd
@@ -1,7 +1,24 @@
 extends Node2D
 @export var gravity:float
 @onready var text:Label = $CanvasLayer/Label
-func _init():
-	return
+@export var stages:PackedStringArray
+@onready var sceneparent:Node = $CanvasLayer/SubViewportContainer/subviewport/Parent
+func _ready() -> void:
+	#var some_scene = load("res://Game.tscn") # returns a PackedScene
+	#var instanced_scene = some_scene.instance() # returns an instance of the scene
+	#sceneparent.add_child(some_scene)
+	ChangeScene(0)
 func _process(delta: float) -> void:
 	text.text = str(gravity) 
+	if Input.is_action_just_pressed("load"):
+		ChangeScene(0)
+
+func ChangeScene(tooo:int):
+	#for n in sceneparent.get_children():
+	#	sceneparent.remove_child(n)
+	#	n.queue_free()
+	var some_scene = load(stages[0]) # returns a PackedScene
+	var instanced_scene = some_scene.instantiate() # returns an instance of the scene
+	sceneparent.add_child(instanced_scene)
+
+	
