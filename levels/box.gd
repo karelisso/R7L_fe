00c61b1540211ee22165de4b22ed8be352474862
karelisso <50169626+karelisso@users.noreply.github.com>
2026-07-03 19:03:27 +0200

    grabby box
    box is like an glitchy ice cube

--- levels/box.gd
+++ levels/box.gd
@@ -4,8 +4,6 @@ var gravity: float
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity
-	if Input.is_action_just_pressed("jump"):
-		velocity.y = -100
 	move_and_slide()
 func SetGravity(f:float):
 	gravity = f
