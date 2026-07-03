31571665fa72464ae474711922eebf0cd567e36a
karelisso <50169626+karelisso@users.noreply.github.com>
2026-07-03 12:52:03 +0200

    snapback 1 is implemented

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -9,15 +9,29 @@ var jump_velocity = -200.0
 var can_walljump = true
 var walljump = true
 @onready var anim = $AnimatedSprite2D
-
+@export var snapback_length = 18 # in buffer_size mult
+#multiply it by interwal to get length i second
+var snapback_interwal = 10 #in frames
+var snapback_counter=0
+var pos_buffer:PackedVector2Array
 func _init() -> void:
 	gravity = 5.0
 	gravity_growth = 0.01
 
+func _process(delta: float) -> void:
+	queue_redraw()
+	get_tree().call_group("weighted","SetGravity",gravity)
 func _physics_process(_delta):
 	var direction := Input.get_axis("left", "right")
 	gravity += gravity_growth
-	
+	snapback_counter+=1
+	if snapback_counter > snapback_interwal:
+		if pos_buffer.size() > snapback_length:
+			pos_buffer.remove_at(0)
+		pos_buffer.append(position)
+		snapback_counter = 0
+	if Input.is_action_just_pressed("load"):
+		position = pos_buffer[0]
 	if not is_on_floor():
 		velocity.y += gravity
 		if walljump and velocity.x ==0 and Input.is_action_just_pressed("jump") and abs(direction) > 0.2:
@@ -46,6 +60,9 @@ func _physics_process(_delta):
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.id == "level_loader":
 		area.call_deferred("loadlevel")
+		#transition between rooms should reset gravity?
+		gravity =5
+		jump_velocity = -200
 		area.get_parent().call_deferred("queue_free")
 	elif area.id == "spring":
 		gravity -= 1
@@ -53,3 +70,6 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 			gravity = 5
 		jump_velocity -= 100
 		area.get_parent().call_deferred("queue_free")
+func _draw() -> void:
+	if pos_buffer.size() > 1:
+		draw_circle(to_local(pos_buffer[0]) ,10,Color(1,0,0,1))
--- scripts/handel_main.gd
+++ scripts/handel_main.gd
@@ -4,7 +4,7 @@ extends Node2D
 @export var stages:PackedStringArray
 @onready var sceneparent:Node = $CanvasLayer/SubViewportContainer/subviewport/Parent
 @onready var player = $CanvasLayer/SubViewportContainer/subviewport/Character
-
+var power:float
 func _ready() -> void:
 	#var some_scene = load("res://Game.tscn") # returns a PackedScene
 	#var instanced_scene = some_scene.instance() # returns an instance of the scene
@@ -13,8 +13,6 @@ func _ready() -> void:
 	ChangeScene(0,313.125,-15.939)
 func _process(delta: float) -> void:
 	text.text = str(gravity) 
-	if Input.is_action_just_pressed("load"):
-		ChangeScene(0,313.125,-15.939)
 func ChangeScene(tooo:int,x:int,y:int):
 	for n in sceneparent.get_children():
 		sceneparent.remove_child(n)
