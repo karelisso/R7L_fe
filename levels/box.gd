95bf972891dae1d185a667646647c6d6ca27ff56
karelisso <50169626+karelisso@users.noreply.github.com>
2026-07-03 16:41:58 +0200

    spawn anywhere

--- levels/box.gd
+++ levels/box.gd
@@ -1,1 +1,10 @@
 extends CharacterBody2D
+var gravity: float
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = -100
+	move_and_slide()
+func SetGravity(f:float):
+	gravity = f
