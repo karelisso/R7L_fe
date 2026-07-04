627fb3d73fd6ef516841f555e37f5ac5c92fcfaa
karelisso <50169626+karelisso@users.noreply.github.com>
2026-07-04 14:37:10 +0200

    .

--- scripts/spring.gd
+++ scripts/spring.gd
@@ -9,9 +9,9 @@ var timer = 0
 @export var wobble_speed = 3.0
 
 func _physics_process(delta: float) -> void:
-	if not get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().is_paused:
-		timer += delta * wobble_speed
-		if timer > 360:
-			timer -= 360
-		offset = Vector2(0, sin(timer) * wobble_strength)
-		mesh.position = offset
+	#if not get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().is_paused:
+	timer += delta * wobble_speed
+	if timer > 360:
+		timer -= 360
+	offset = Vector2(0, sin(timer) * wobble_strength)
+	mesh.position = offset
