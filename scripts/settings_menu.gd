a5cdcaef6f1594ae022e6c2b1f0ee69fbd116c70
Kurby <andras.bukovics@gmail.com>
2026-07-05 09:41:39 +0200

    Made the UI work with keyboard and controller

--- scripts/settings_menu.gd
+++ scripts/settings_menu.gd
@@ -5,19 +5,22 @@ extends Control
 @onready var volume_slider: HSlider = $CanvasLayer/BoxContainer/VolumeSlider
 
 func _ready() -> void:
+	volume_slider.grab_focus()
 	background.modulate = Color(0, 0, 0, 1)
 	volume_slider.value = AudioServer.get_bus_volume_linear(AudioServer.get_bus_index("Master"))
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("pause"):
+	if Input.is_action_just_pressed("back"):
 		#var instance = scene.instantiate()
 		#add_sibling(instance)
+		get_parent()._ready()
 		call_deferred("queue_free")
 
 func _on_back_button_button_down() -> void:
-	call_deferred("queue_free")
 	#var instance = scene.instantiate()
 	#add_sibling(instance)
+	get_parent()._ready()
+	call_deferred("queue_free")
 
 func _on_volume_slider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Master"), value)
