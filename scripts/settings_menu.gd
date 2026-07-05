e36cc5da26ed7c0507a4e58716dd42c961c902ff
Kurby <andras.bukovics@gmail.com>
2026-07-05 08:49:27 +0200

    Fixed labels rendering over char and cleaned up layers a bit

--- scripts/settings_menu.gd
+++ scripts/settings_menu.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 	background.modulate = Color(0, 0, 0, 0.5)
 	volume_slider.value = AudioServer.get_bus_volume_linear(AudioServer.get_bus_index("Master"))
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
 		var instance = scene.instantiate()
 		add_sibling(instance)
