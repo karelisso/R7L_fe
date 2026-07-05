9ddd681a70700d783ec013dfd55e0f541dec08fa
Kurby <andras.bukovics@gmail.com>
2026-07-05 15:32:44 +0200

    Gui fix

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -4,13 +4,14 @@ extends Control
 @onready var settings_menu = preload("res://scenes/settings_menu.tscn")
 @onready var background: Panel = $CanvasLayer/background
 @onready var back_button: TextureButton = $CanvasLayer/BoxContainer/BackButton
+var is_settings_open = false
 
 func _ready() -> void:
 	back_button.grab_focus()
 	background.modulate = Color(0, 0, 0, 0.5)
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("back"):
+	if Input.is_action_just_pressed("back") and not is_settings_open:
 		get_parent().toggle_pause()
 		call_deferred("queue_free")
 
@@ -22,6 +23,7 @@ func _on_main_menu_button_button_down() -> void:
 
 func _on_settings_button_button_down() -> void:
 	settings_menu = load("res://scenes/settings_menu.tscn")
+	is_settings_open = true
 	var instance = settings_menu.instantiate()
 	add_child(instance)
 	#instance.scene = load("res://scenes/pause_menu.tscn")
